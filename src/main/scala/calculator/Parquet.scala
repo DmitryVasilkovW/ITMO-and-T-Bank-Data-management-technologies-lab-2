5e782e11f5527dc7903aecc07005288aa469b262
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-09-15 02:07:35 +0300

    refactor: code style

--- src/main/scala/calculator/Parquet.scala
+++ src/main/scala/calculator/Parquet.scala
@@ -6,12 +6,12 @@ object Parquet {
 
   def calculate(building: Building): Int =
     building match {
-      case Economy(length, width, height, floorNumber) =>
-        length * width * height + floorNumber * 10000
       case Premium(length, width, height, floorNumber) if floorNumber < 5 =>
         Math.pow(3, floorNumber).toInt * (length + width + height)
       case Premium(length, width, height, floorNumber) =>
         Math.pow(2, floorNumber).toInt * (length + width + height)
+      case Economy(length, width, height, floorNumber) =>
+        length * width * height + floorNumber * 10000
     }
 
 }
--- src/main/scala/fibonacci/Fibonacci.scala
+++ src/main/scala/fibonacci/Fibonacci.scala
@@ -1,11 +1,13 @@
 package fibonacci
 
+import scala.annotation.tailrec
+
 object Fibonacci {
 
   def fibonacci(limit: Long): BigInt =
     fibTailRec(limit, BigInt(0), BigInt(1))
 
-  @scala.annotation.tailrec
+  @tailrec
   private def fibTailRec(n: Long, a: BigInt, b: BigInt): BigInt =
     if (n == 0) a
     else fibTailRec(n - 1, b, a + b)
